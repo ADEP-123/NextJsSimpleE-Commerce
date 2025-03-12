f3882034a0c1c8ee58f585ecc1c6e7afc3b7533c
Andres Elizalde <andep210399@gmail.com>
2025-03-12 16:03:58 +0200

    feat: :sparkles: header module and its styles

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function SomethingPage({params}) {
+    return (
+        <main>
+            <h1>Something page </h1>
+        </main>
+    )
+}
---
+++ app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+    return (
+        <main>
+            <h1>Meals page</h1>
+            <Link href="/meals/something">Something</Link>
+        </main>
+    )
+}
