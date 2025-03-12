81c757131b22bb79faccb94a400b972c1e50d7aa
Andres Elizalde <andep210399@gmail.com>
2025-03-12 16:15:53 +0200

    feat: :sparkles: spliting main header background into a new component

---
+++ components/main-header/main-header.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from '@/assets/logo.png'
+import styles from './main-header.module.css'
+import MainHeaderBackground from "./main-heder-background";
+
+export default function MainHeader() {
+    return (
+        <>
+            <MainHeaderBackground/>
+            <header className={styles.header}>
+                <Link href="/" className={styles.logo}>
+                    <Image src={logoImg} alt="A plate with food on it" priority />
+                    NextLevel Food
+                </Link>
+
+                <nav className={styles.nav}>
+                    <ul>
+                        <li> <Link href='/meals'>Browse meals</Link></li>
+                        <li> <Link href='/community'>Foodies Community</Link></li>
+                    </ul>
+                </nav>
+            </header >
+        </>
+    )
+}
